0295c3ee74d3e6c6fa2aa37ac1374e5eca3ee80b
DayannaBeltran <carmen.beltran248565@potros.itson.edu.mx>
2026-06-22 00:00:28 -0700

    Add files via upload

---
+++ GameVault/GameVault/src/main/java/com/example/GameVault/model/Juego.java
@@ -0,0 +1,28 @@
+package com.example.GameVault.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Genera Getters, Setters, toString, etc. (Gracias a Lombok)
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="juegos")
+public class Juego {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    @Column(nullable = false)
+    private String descripcion;
+
+    // Almacenaremos el nombre del archivo o la URL de la imagen
+    @Column(nullable = false)
+    private String portadaUrl;
+}
---
+++ GameVault/GameVault/src/main/java/com/example/GameVault/service/JuegoService.java
@@ -0,0 +1,61 @@
+package com.example.GameVault.service;
+
+import com.example.GameVault.model.Juego;
+import com.example.GameVault.repository.JuegoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class JuegoService {
+
+    @Autowired
+    private JuegoRepository juegoRepository;
+
+    private static final String UPLOAD_DIR ="src/main/resources/static/uploads";
+
+    public List<Juego> listarTodos(){
+        return juegoRepository.findAll();
+    }
+
+    public void guardarJuego(Juego juego, MultipartFile file){
+            String nombreArchivo= "default.png";
+
+            if(!file.isEmpty()) {
+                nombreArchivo= guardarImagenEnLocal(file);
+            }
+            juego.setPortadaUrl(nombreArchivo);
+
+            juegoRepository.save(juego);
+
+    }
+
+    private String guardarImagenEnLocal(MultipartFile file){
+            try {
+                Path uploadPath = Paths.get(UPLOAD_DIR);
+
+                if (!Files.exists(uploadPath)) {
+                    Files.createDirectories(uploadPath);
+                }
+
+                String nombreArchivo = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+
+                Path filePath = uploadPath.resolve(nombreArchivo);
+
+                Files.copy(file.getInputStream(), filePath);
+
+                return nombreArchivo;
+            }catch (IOException e){
+                e.printStackTrace();
+                return "default.png";
+            }
+        }
+    }
+
